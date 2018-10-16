572145c3bd841604c1f8a38e22d2af235de15a68
jerry <568384493@qq.com>
2018-10-17 00:23:23 +0800

    mysql操作

---
+++ MySqlStorage/connectionMysql.py
@@ -0,0 +1,11 @@
+# 连接mysql
+
+import pymysql
+
+db = pymysql.connect(host='localhost',user='root', password='123456', port=3306)
+cursor = db.cursor()
+cursor.execute('SELECT VERSION()')
+data = cursor.fetchone()
+print('Database version:', data)
+cursor.execute("CREATE DATABASE spiders DEFAULT CHARACTER SET utf8")
+db.close()
