07b43b4f4db8af12ee49d2484438aff78afb14b6
hcw <897796171@qq.com>
2018-10-18 18:21:50 +0800

    mysql 操作

--- MySqlStorage/Insert.py
+++ MySqlStorage/Insert.py
@@ -6,7 +6,8 @@ import pymysql
 id = '20120001'
 user = 'Bob'
 age = 20
-db = pymysql.connect(host='localhot', user='root', password='123456', port='3306', db='spiders')
+
+db = pymysql.connect(host='localhost', user='root', password='123456', port=3306, db='spiders')
 cursor = db.cursor()
 sql = 'INSERT INTO students(id, name, age) values(%s, %s, %s)'
 try:
