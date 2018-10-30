a3420e0650fc68fd73c300f85f3149d29b873076
jerry <568384493@qq.com>
2018-10-31 00:00:06 +0800

    添加 爬虫 抓取 拉钩

---
+++ AJAX/lagoujson.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+from urllib import request
+# json解析库，对应到lxml
+import json
+
+import jsonpath
+
+url = "http://www.lagou.com/lbs/getAllCitySearchLabels.json"
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36"}
+req = request.Request(url)
+response = request.urlopen(req)
+html = response.read()
+unicodestr = json.loads(html)
+# Python形式的列表
+city_list = jsonpath.jsonpath(unicodestr, "$..name")
+
+# for item in city_list:
+#    print item
+
+# dumps()默认中文为ascii编码格式，ensure_ascii默认为Ture
+# 禁用ascii编码格式，返回的Unicode字符串，方便使用
+array = json.dumps(city_list, ensure_ascii=False)
+# json.dumps(city_list)
+# array = json.dumps(city_list)
+
+with open("lagoucity.json", "w") as f:
+    f.write(array.encode('utf-8').decode('utf-8'))
