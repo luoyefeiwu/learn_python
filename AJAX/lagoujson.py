1ea9174c0aab40e07b7634ce486b282d4515a68a
jerry <568384493@qq.com>
2018-11-01 22:48:09 +0800

    '抓取拉钩的城市数据'

--- AJAX/lagoujson.py
+++ AJAX/lagoujson.py
@@ -8,7 +8,7 @@ import jsonpath
 url = "http://www.lagou.com/lbs/getAllCitySearchLabels.json"
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36"}
-req = request.Request(url)
+req = request.Request(url, headers=headers)
 response = request.urlopen(req)
 html = response.read()
 unicodestr = json.loads(html)
@@ -21,8 +21,7 @@ city_list = jsonpath.jsonpath(unicodestr, "$..name")
 # dumps()默认中文为ascii编码格式，ensure_ascii默认为Ture
 # 禁用ascii编码格式，返回的Unicode字符串，方便使用
 array = json.dumps(city_list, ensure_ascii=False)
-# json.dumps(city_list)
-# array = json.dumps(city_list)
-
+print(array)
+print(type(array))
 with open("lagoucity.json", "w") as f:
-    f.write(array.encode('utf-8').decode('utf-8'))
+    f.write(array)
